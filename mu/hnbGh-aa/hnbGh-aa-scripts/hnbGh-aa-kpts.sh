089b177d4496bd5cefeba9b766a27005d77aa203
reychino-heisenberg <reychino@gmail.com>
2016-12-22 18:44:37 -0600

    Cheking kpts convergence for aa structure.

--- mu/hnbGh-aa/hnbGh-aa-scripts/hnbGh-aa-kpts.sh
+++ mu/hnbGh-aa/hnbGh-aa-scripts/hnbGh-aa-kpts.sh
@@ -4,9 +4,47 @@
 strc=hnbGh-aa
 # kpts=( 2452 3202 4052 5002 6052 7202 8452 9802 11252 )
 # kpts=( 6052 7202 8452 9802 11252 )
-kpts=( 7202 8452 9802 11252 )
+# kpts=( 7202 8452 9802 11252 )
+kpts=( 9802 11252 )
 pws=10992
 ################################################################################
+
+
+all_responses.sh -w total -m 5002_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xxxx xxxy xxxz xxyx xxyy xxyz xxzx xxzy xxzz" -b 0.03
+all_responses.sh -w total -m 5002_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xyxx xyxy xyxz xyyx xyyy xyyz xyzx xyzy xyzz" -b 0.03
+all_responses.sh -w total -m 5002_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xzxx xzxy xzxz xzyx xzyy xzyz xzzx xzzy xzzz" -b 0.03
+all_responses.sh -w total -m 5002_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yxxx yxxy yxxz yxyx yxyy yxyz yxzx yxzy yxzz" -b 0.03
+all_responses.sh -w total -m 5002_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yyxx yyxy yyxz yyyx yyyy yyyz yyzx yyzy yyzz" -b 0.03
+all_responses.sh -w total -m 5002_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yzxx yzxy yzxz yzyx yzyy yzyz yzzx yzzy yzzz" -b 0.03
+all_responses.sh -w total -m 5002_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zxxx zxxy zxxz zxyx zxyy zxyz zxzx zxzy zxzz" -b 0.03
+all_responses.sh -w total -m 5002_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zyxx zyxy zyxz zyyx zyyy zyyz zyzx zyzy zyzz" -b 0.03
+all_responses.sh -w total -m 5002_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zzxx zzxy zzxz zzyx zzyy zzyz zzzx zzzy zzzz" -b 0.03
+all_responses.sh -w total -m 5002_75-spin -s 0 -o 1 -v 18 -c 32 -r  3 -t "xxy yxy zxy" -b 0.03
+all_responses.sh -w total -m 5002_75-spin -s 0 -o 1 -v 18 -c 32 -r  1 -t "xx yy zz" -b 0.03
+
+all_responses.sh -w total -m 6052_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xxxx xxxy xxxz xxyx xxyy xxyz xxzx xxzy xxzz" -b 0.03
+all_responses.sh -w total -m 6052_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xyxx xyxy xyxz xyyx xyyy xyyz xyzx xyzy xyzz" -b 0.03
+all_responses.sh -w total -m 6052_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xzxx xzxy xzxz xzyx xzyy xzyz xzzx xzzy xzzz" -b 0.03
+all_responses.sh -w total -m 6052_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yxxx yxxy yxxz yxyx yxyy yxyz yxzx yxzy yxzz" -b 0.03
+all_responses.sh -w total -m 6052_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yyxx yyxy yyxz yyyx yyyy yyyz yyzx yyzy yyzz" -b 0.03
+all_responses.sh -w total -m 6052_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yzxx yzxy yzxz yzyx yzyy yzyz yzzx yzzy yzzz" -b 0.03
+all_responses.sh -w total -m 6052_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zxxx zxxy zxxz zxyx zxyy zxyz zxzx zxzy zxzz" -b 0.03
+all_responses.sh -w total -m 6052_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zyxx zyxy zyxz zyyx zyyy zyyz zyzx zyzy zyzz" -b 0.03
+all_responses.sh -w total -m 6052_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zzxx zzxy zzxz zzyx zzyy zzyz zzzx zzzy zzzz" -b 0.03
+all_responses.sh -w total -m 6052_75-spin -s 0 -o 1 -v 18 -c 32 -r  3 -t "xxy yxy zxy" -b 0.03
+all_responses.sh -w total -m 6052_75-spin -s 0 -o 1 -v 18 -c 32 -r  1 -t "xx yy zz" -b 0.03
+
+all_responses.sh -w total -m 8452_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xxxx xxxy xxxz xxyx xxyy xxyz xxzx xxzy xxzz" -b 0.03
+all_responses.sh -w total -m 8452_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xyxx xyxy xyxz xyyx xyyy xyyz xyzx xyzy xyzz" -b 0.03
+all_responses.sh -w total -m 8452_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xzxx xzxy xzxz xzyx xzyy xzyz xzzx xzzy xzzz" -b 0.03
+all_responses.sh -w total -m 8452_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yxxx yxxy yxxz yxyx yxyy yxyz yxzx yxzy yxzz" -b 0.03
+all_responses.sh -w total -m 8452_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yyxx yyxy yyxz yyyx yyyy yyyz yyzx yyzy yyzz" -b 0.03
+all_responses.sh -w total -m 8452_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yzxx yzxy yzxz yzyx yzyy yzyz yzzx yzzy yzzz" -b 0.03
+all_responses.sh -w total -m 8452_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zxxx zxxy zxxz zxyx zxyy zxyz zxzx zxzy zxzz" -b 0.03
+all_responses.sh -w total -m 8452_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zyxx zyxy zyxz zyyx zyyy zyyz zyzx zyzy zyzz" -b 0.03
+all_responses.sh -w total -m 8452_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zzxx zzxy zzxz zzyx zzyy zzyz zzzx zzzy zzzz" -b 0.03
+all_responses.sh -w total -m 8452_75-spin -s 0 -o 1 -v 18 -c 32 -r  3 -t "xxy yxy zxy" -b 0.03
+all_responses.sh -w total -m 8452_75-spin -s 0 -o 1 -v 18 -c 32 -r  1 -t "xx yy zz" -b 0.03
 
 for i in ${kpts[@]}; do
     START=$(date)
