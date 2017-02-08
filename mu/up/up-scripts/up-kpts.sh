e17ac5d30f6d1dcd97bc22ccd09da09740a43bdd
reychino-heisenberg <reychino@gmail.com>
2017-02-08 09:40:35 -0600

    -

---
+++ mu/up/up-scripts/up-kpts.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+############################## T O   C H A N G E ###############################
+strc=hnbGh-ab
+kpts=( 12802 11252 )
+pws=2359 
+################################################################################
+
+for i in ${kpts[@]}; do
+    START=$(date)
+
+    # run_tiniba.sh -r run -k "$i" -N 0 -x 2 -C 12 -P "$pws" -w -e -p -s
+    run_tiniba.sh -r run -k "$i" -N 0 -x 2 -C 12 -P "$pws" -s
+
+    all_responses.sh
+    all_responses.sh -w total -m "$i"_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xxxx xxxy xxxz xxyx xxyy xxyz xxzx xxzy xxzz" -b 0.03
+    all_responses.sh -w total -m "$i"_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xyxx xyxy xyxz xyyx xyyy xyyz xyzx xyzy xyzz" -b 0.03
+    all_responses.sh -w total -m "$i"_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "xzxx xzxy xzxz xzyx xzyy xzyz xzzx xzzy xzzz" -b 0.03
+    all_responses.sh -w total -m "$i"_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yxxx yxxy yxxz yxyx yxyy yxyz yxzx yxzy yxzz" -b 0.03
+    all_responses.sh -w total -m "$i"_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yyxx yyxy yyxz yyyx yyyy yyyz yyzx yyzy yyzz" -b 0.03
+    all_responses.sh -w total -m "$i"_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "yzxx yzxy yzxz yzyx yzyy yzyz yzzx yzzy yzzz" -b 0.03
+    all_responses.sh -w total -m "$i"_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zxxx zxxy zxxz zxyx zxyy zxyz zxzx zxzy zxzz" -b 0.03
+    all_responses.sh -w total -m "$i"_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zyxx zyxy zyxz zyyx zyyy zyyz zyzx zyzy zyzz" -b 0.03
+    all_responses.sh -w total -m "$i"_75-spin -s 0 -o 1 -v 18 -c 32 -r 48 -t "zzxx zzxy zzxz zzyx zzyy zzyz zzzx zzzy zzzz" -b 0.03
+    all_responses.sh -w total -m "$i"_75-spin -s 0 -o 1 -v 18 -c 32 -r  3 -t "xxy yxy zxy" -b 0.03
+    all_responses.sh -w total -m "$i"_75-spin -s 0 -o 1 -v 18 -c 32 -r  1 -t "xx yy zz" -b 0.03
+
+    END=$(date)
+    ssh cassandra "echo -e 'Calculations completed: $i kpts for $strc \nStarted at $START \nFinished at $END \n\nArray of ktps: \n${kpts[@]} ' | mail -s '$i kpts for $strc' reychino@gmail.com"
+
+    sleep 30m
+done
+
+run_tiniba.sh -r erase
