cc177f126f5abaffd7181dc60020147daef130ce
reychino-heisenberg <reychino@gmail.com>
2017-02-15 19:10:51 -0600

    Using new vab.sh script.

---
+++ mu/res-analysis.sh
@@ -0,0 +1,10 @@
+
+p   'hnbGh-aa/hnbGh-aa-res-0-4-2001pts/v.sm_0.03_yx_14452_75-spin_scissor_0_Nc_32_ang_00' u 1:($2*29.98) w l lw 2 t "aa yx" # intense
+rep 'hnbGh-ab/hnbGh-ab-res-0-4-2001pts/v.sm_0.03_xz_14452_75-spin_scissor_0_Nc_32_ang_00' u 1:($2*29.98) w l lw 2 t "ab xz" # intense
+rep 'alt/alt-res-0-4-2001pts/v.sm_0.03_zx_14452_65-spin_scissor_0_Nc_32_ang_00'           u 1:($2*29.98) w l lw 2 t "alt zx" # intense
+rep 'up/up-res-0-4-2001pts/v.sm_0.03_zx_12802_40-spin_scissor_0_Nc_32_ang_00'             u 1:($2*29.98) w l lw 2 t "up zx" # intense
+rep 'gaas/gaas-res-0-4-2001pts/v.sm_0.03_xx_5216_15-spin_scissor_0_Nc_24_ang_00'          u 1:($2*29.98) w l lw 2 t "gaas xx" # intense
+rep 'cdse/cdse-res-0-4-2001pts/v.sm_0.03_zz_3234_15-spin_scissor_0_Nc_16_ang_00'          u 1:($2*29.98) w l lw 2 t "cdse zz" # intense
+
+
+
