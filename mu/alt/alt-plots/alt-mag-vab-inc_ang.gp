53cbf4bfceec9daf38e9ed573566224ad3a704ac
reychino-heisenberg <reychino@gmail.com>
2017-04-07 08:55:50 -0500

    Making correction in V_{s^x}.

--- mu/alt/alt-plots/alt-mag-vab-inc_ang.gp
+++ mu/alt/alt-plots/alt-mag-vab-inc_ang.gp
@@ -15,7 +15,50 @@ f=pf*sl
 set label 'C$_{16}$H$_{8}$-alt' at graph 0.1, 0.0, 0.95 
 
 
-################################ [0.65:1.8] ####################################
+################################ FixVel [0.65:1.8] #############################
+
+# set xrange [0.65:1.0]
+
+# set view 44,45
+# set hidden3d
+# set xtics 0.7, 0.05, 1.0
+# set ytics 30
+
+# set zrange [0:35]
+# set ztics 5
+# set pm3d scansbackward
+# set pm3d implicit at s
+# set cbrange [-1:30]
+# # unset ztics
+
+# set ylabel 'Polarization angle $\alpha$ [deg.]'       rotate parallel offset 1.3,-1.0, 0.0
+# set xlabel 'Photon Energy [eV]'              rotate parallel offset 0.0,-1.5, 0.0
+# set zlabel '$|\mathcal{V}^{\mathrm{x}}|$ [Km/s]' rotate parallel offset 1.0, 0.0, 0.0 
+# set output "alt-magvxb-incang-4545.mp"
+# sp  '../alt-res-0-4-2001pts/magv.sm_0.03_xb_14452_65-spin_scissor_0_Nc_32_incang_0-180-step5' u 1:3:($2*f) w l lw 0.6 lt 8
+
+
+# set view 44,45
+# set hidden3d
+# set xtics 0.7, 0.05, 1.0
+# set ytics 30
+
+# set zrange [0:60]
+# set ztics 10
+# set pm3d scansbackward
+# set pm3d implicit at s
+# set cbrange [-2:50]
+# # unset ztics
+
+# set ylabel 'Polarization angle $\alpha$ [deg.]'       rotate parallel offset 1.3,-1.0, 0.0
+# set xlabel 'Photon Energy [eV]'              rotate parallel offset 0.0,-1.5, 0.0
+# set zlabel '$|\mathcal{V}^{\mathrm{y}}|$ [Km/s]' rotate parallel offset 1.0, 0.0, 0.0 
+# set output "alt-magvyb-incang-4545.mp"
+# sp  '../alt-res-0-4-2001pts/magv.sm_0.03_yb_14452_65-spin_scissor_0_Nc_32_incang_0-180-step5' u 1:3:($2*f) w l lw 0.65 lt 8
+
+
+############################### FixSpin [0.65:1.8] #############################
+
 set xrange [0.65:1.0]
 
 set view 44,45
@@ -32,9 +75,9 @@ set cbrange [-1:30]
 
 set ylabel 'Polarization angle $\alpha$ [deg.]'       rotate parallel offset 1.3,-1.0, 0.0
 set xlabel 'Photon Energy [eV]'              rotate parallel offset 0.0,-1.5, 0.0
-set zlabel '$|\cal{V}^{\mathrm{x}}|$ [Km/s]' rotate parallel offset 1.0, 0.0, 0.0 
+set zlabel '$|\mathcal{V}_{\sigma^{\mathrm{x}}}|$ [Km/s]' rotate parallel offset 1.0, 0.0, 0.0 
 set output "alt-magvxb-incang-4545.mp"
-sp  '../alt-res-0-4-2001pts/magv.sm_0.03_xb_14452_65-spin_scissor_0_Nc_32_incang_0-180-step5' u 1:3:($2*f) w l lw 0.6 lt 8
+sp  '../alt-res-0-4-2001pts/magsv.sm_0.03_ay_14452_65-spin_scissor_0_Nc_32_incang_0-180-step5' u 1:3:($2*f) w l lw 0.6 lt 8
 
 
 set view 44,45
@@ -51,9 +94,9 @@ set cbrange [-2:50]
 
 set ylabel 'Polarization angle $\alpha$ [deg.]'       rotate parallel offset 1.3,-1.0, 0.0
 set xlabel 'Photon Energy [eV]'              rotate parallel offset 0.0,-1.5, 0.0
-set zlabel '$|\cal{V}^{\mathrm{y}}|$ [Km/s]' rotate parallel offset 1.0, 0.0, 0.0 
+set zlabel '$|\mathcal{V}_{\sigma^{\mathrm{y}}}|$ [Km/s]' rotate parallel offset 1.0, 0.0, 0.0 
 set output "alt-magvyb-incang-4545.mp"
-sp  '../alt-res-0-4-2001pts/magv.sm_0.03_yb_14452_65-spin_scissor_0_Nc_32_incang_0-180-step5' u 1:3:($2*f) w l lw 0.65 lt 8
+sp  '../alt-res-0-4-2001pts/magsv.sm_0.03_ax_14452_65-spin_scissor_0_Nc_32_incang_0-180-step5' u 1:3:($2*f) w l lw 0.65 lt 8
 
 
 
--- mu/hnbGh-ab/hnbGh-ab-scripts/hnbGh-ab-magsvab-inang.sh
+++ mu/hnbGh-ab/hnbGh-ab-scripts/hnbGh-ab-magsvab-inang.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+angles=( 000 005 010 015 020 025 030 035 040 045 050 055 060 065 070 075 080 085 090 095 100 105 110 115 120 125 130 135 140 145 150 155 160 165 170 175 180 )
+
+############################## V_{s^x} #########################################
 
 outputxkk=magsv.sm_0.03_ax_14452_75-spin_scissor_0_Nc_32_incang_0-180-step5
 outputxsm=magsv.kk_ax_14452_75-spin_scissor_0_Nc_32_incang_0-180-step5
@@ -9,9 +12,16 @@ inputsxkk=( v.kk_xx_14452_75-spin_scissor_0_Nc_32_ang_
 inputsxsm=( v.sm_0.03_xx_14452_75-spin_scissor_0_Nc_32_ang_  
             v.sm_0.03_yx_14452_75-spin_scissor_0_Nc_32_ang_  )
 
+echo -e "E              Vsx               PolAng" > $outputxkk
+echo -e "E              Vsx               PolAng" > $outputxsm
+for i in ${angles[@]}; do
+    paste "${inputsxsm[0]}""$i"  "${inputsxsm[1]}""$i" | awk '{printf("%1.5E    %1.6E    %3.0f\n"), $1, sqrt($2*$2 + $4*$4), '"$i"' }' >> $outputxkk
+    echo "" >> $outputxkk
+    paste "${inputsxkk[0]}""$i"  "${inputsxkk[1]}""$i" | awk '{printf("%1.5E    %1.6E    %3.0f\n"), $1, sqrt($2*$2 + $4*$4), '"$i"' }' >> $outputxsm
+    echo "" >> $outputykk
+done
 
-
-
+############################## V_{s^y} #########################################
 
 outputykk=magsv.sm_0.03_ay_14452_75-spin_scissor_0_Nc_32_incang_0-180-step5
 outputysm=magsv.kk_ay_14452_75-spin_scissor_0_Nc_32_incang_0-180-step5
@@ -21,24 +31,30 @@ inputsykk=( v.kk_xy_14452_75-spin_scissor_0_Nc_32_ang_
 inputsysm=( v.sm_0.03_xy_14452_75-spin_scissor_0_Nc_32_ang_  
             v.sm_0.03_yy_14452_75-spin_scissor_0_Nc_32_ang_  )
 
-
-angles=( 000 005 010 015 020 025 030 035 040 045 050 055 060 065 070 075 080 085 090 095 100 105 110 115 120 125 130 135 140 145 150 155 160 165 170 175 180 )
-
-rm "$outputxkk" "$outputxsm" "$outputykk" "$outputysm" > /dev/null 2>&1
-
+echo -e "E              Vsy               PolAng" > $outputykk
+echo -e "E              Vsy               PolAng" > $outputysm
 for i in ${angles[@]}; do
-    paste "${inputsxsm[0]}""$i"  "${inputsxsm[1]}""$i" | awk '{printf("%1.5E    %1.6E    %3.0f\n"), $1, sqrt($2*$2 + $4*$4), '"$i"' }' >> $outputxkk
-    echo "" >> $outputxkk
     paste "${inputsysm[0]}""$i"  "${inputsysm[1]}""$i" | awk '{printf("%1.5E    %1.6E    %3.0f\n"), $1, sqrt($2*$2 + $4*$4), '"$i"' }' >> $outputykk
-    echo "" >> $outputykk
-done
-
-for i in ${angles[@]}; do
-    paste "${inputsxkk[0]}""$i"  "${inputsxkk[1]}""$i" | awk '{printf("%1.5E    %1.6E    %3.0f\n"), $1, sqrt($2*$2 + $4*$4), '"$i"' }' >> $outputxsm
     echo "" >> $outputxsm
     paste "${inputsykk[0]}""$i"  "${inputsykk[1]}""$i" | awk '{printf("%1.5E    %1.6E    %3.0f\n"), $1, sqrt($2*$2 + $4*$4), '"$i"' }' >> $outputysm
     echo "" >> $outputysm
 done
 
+############################## V_{s^z} #########################################
 
+outputzkk=magsv.sm_0.03_az_14452_75-spin_scissor_0_Nc_32_incang_0-180-step5
+outputzsm=magsv.kk_az_14452_75-spin_scissor_0_Nc_32_incang_0-180-step5
 
+inputszkk=( v.kk_xz_14452_75-spin_scissor_0_Nc_32_ang_  
+            v.kk_yz_14452_75-spin_scissor_0_Nc_32_ang_  )
+inputszsm=( v.sm_0.03_xz_14452_75-spin_scissor_0_Nc_32_ang_  
+            v.sm_0.03_yz_14452_75-spin_scissor_0_Nc_32_ang_  )
+
+echo -e "E              Vsz               PolAng" > $outputzkk
+echo -e "E              Vsz               PolAng" > $outputzsm
+for i in ${angles[@]}; do
+    paste "${inputsysm[0]}""$i"  "${inputsysm[1]}""$i" | awk '{printf("%1.5E    %1.6E    %3.0f\n"), $1, sqrt($2*$2 + $4*$4), '"$i"' }' >> $outputykk
+    echo "" >> $outputxsm
+    paste "${inputsykk[0]}""$i"  "${inputsykk[1]}""$i" | awk '{printf("%1.5E    %1.6E    %3.0f\n"), $1, sqrt($2*$2 + $4*$4), '"$i"' }' >> $outputysm
+    echo "" >> $outputysm
+done
