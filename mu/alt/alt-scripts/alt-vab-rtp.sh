a58767d774de547e22a0118a20f85485ca0948c7
reychino-heisenberg <reychino@gmail.com>
2017-04-20 14:21:18 -0500

    Making alt-vab-rtp.sh with deg and all positive angles.

--- mu/alt/alt-scripts/alt-vab-rtp.sh
+++ mu/alt/alt-scripts/alt-vab-rtp.sh
@@ -8,6 +8,8 @@ sc=0
 nc=32
 sm=0.03
 
+deg=180/3.141592
+
 
 xx=v.sm_"$sm"_xx_"$kp"_"$ec"-spin_scissor_"$sc"_Nc_"$nc"_ang_"$an" 
 xy=v.sm_"$sm"_xy_"$kp"_"$ec"-spin_scissor_"$sc"_Nc_"$nc"_ang_"$an" 
@@ -22,6 +24,7 @@ outy=vab-rtp.sm_"$sm"_y_"$kp"_"$ec"-spin_scissor_"$sc"_Nc_"$nc"_ang_"$an"
 
 paste $xx $xy $xz  | awk '{ printf("%1.6E    %1.6E    %1.6E    %1.6E    %1.6E\n", $1, $2, $4, $6, sqrt($2*$2 + $4*$4 + $6*$6) ) }' > .temp1
 paste .temp1 | awk '{if($5 != 0) {printf("%1.6E    %1.6E\n", 2*atan2((sqrt(1-($4/($5))^2)),(1+($4/($5)))), atan2($3,$2) ) ;} else if($5==0) {printf("%1.6E    %1.6E\n"), 0,0 } }' > .temp2
+paste .temp1 | awk '{if($2 < 0) {printf("%010.6f    %010.6f\n", ($1*'"$deg"'), ($2*'"$deg"')+180) ;} else {printf("%010.6f    %010.6f\n"), ($1*'"$deg"'), $2*'"$deg"' } }' > .deg1
 
 echo -e "Energy          V^{ax}          V^{ay}          V^{az}          r               polar           azimuth" > $outx
 paste .temp1 .temp2 | awk '{printf("%1.6E    %1.6E    %1.6E    %1.6E    %1.6E    %1.6E    %1.6E\n", $1, $2, $3, $4, $5, $6, $7) }'  >> $outx
